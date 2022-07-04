9b32ec3663484e06a704a97a8e1d563648a1935d
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-07-04 16:57:55 -0700

    Remove redundant try...except's

--- src/grades.py
+++ src/grades.py
@@ -4,15 +4,13 @@ import time
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from setup import CWL, PASSWORD, EMAIL_LIST, EMAIL_SEND_DELAY, CHECK_INTERVAL, SEND_DATA
+from setup import CWL, PASSWORD, EMAIL_LIST, EMAIL_SEND_DELAY, CHECK_INTERVAL, SEND_DATA, courses
 
 # Chrome driver releases: https://chromedriver.storage.googleapis.com/index.html
 s = Service('chromedriver.exe')
 
 
-def gradesCheck():
-    # Accepts a list with each entry as "COURSE_CODE #" E.g. "MATH 100"
-    courses = ["PHIL 120", "MATH 100", "MATH 152"]
+def gradesCheck(courses):
     foundCourses = []
     found = 0
     origLength = len(courses)
@@ -23,20 +21,17 @@ def gradesCheck():
         driver.get("https://cas.id.ubc.ca/ubc-cas/login?TARGET=https%3A%2F%2Fssc.adm.ubc.ca%2Fsscportal%2Fservlets%2FSSCMain.jsp%3Ffunction%3DSessGradeRpt")
 
         try:
+            # Fill in username field
             search = driver.find_element(by=By.NAME, value="username")
             search.send_keys(CWL)
-        except:
-            print("errorName")
 
-        try:
+            # Fill in password field and submit login
             search = driver.find_element(by=By.NAME, value="password")
             search.send_keys(PASSWORD)
             search.send_keys(Keys.RETURN)
             time.sleep(6)
-        except:
-            print("errorPass")
 
-        try:
+            # Search through grades and find the right course(s)
             driver.switch_to.frame("iframe-main")
             for course in courses:
                 search = driver.find_element(
@@ -49,6 +44,7 @@ def gradesCheck():
                         break
                 parent = search.find_element(by=By.XPATH, value="..")
                 grade = parent.find_element(by=By.XPATH, value="td[3]")
+                # Check if there's a grade yet
                 if grade.text == " " or grade.text == "":
                     print("No grade for " + course + " yet")
                 else:
@@ -63,11 +59,12 @@ def gradesCheck():
             foundCourses = []
 
         except Exception as e:
-            print("errorGrades: " + str(e))
+            print("Error Checking SSC: " + str(e))
 
         driver.quit()
 
         if found < origLength:
+            print("Delay before retrying check")
             time.sleep(CHECK_INTERVAL)
 
     print("All grades found!")
@@ -88,45 +85,30 @@ def sendEmail(course, gradeValue):
             search = driver.find_element(by=By.NAME, value="username")
             search.click()
             search.send_keys(CWL + "@student.ubc.ca")
-        except:
-            print("errorEmail")
 
-        try:
             search = driver.find_element(by=By.NAME, value="password")
             search.send_keys(PASSWORD)
             search.send_keys(Keys.RETURN)
             time.sleep(3)
-        except:
-            print("errorPass")
 
-        try:
             search = driver.find_element(
                 by=By.XPATH, value="//*[@title = 'Write a new message (N)']")
             search.click()
             time.sleep(2)
-        except:
-            print("errorNewEmail")
 
-        try:
             search = driver.find_element(
                 by=By.XPATH, value="//*[@aria-label = 'To']")
             for email in EMAIL_LIST:
                 search.send_keys(email)
                 search.send_keys(Keys.TAB)
             time.sleep(2)
-        except:
-            print("errorTo")
 
-        try:
             search = driver.find_element(
                 by=By.XPATH, value="//*[@aria-label = 'Subject,']")
             search.click()
             search.send_keys("Course Grade Release - " + course)
             time.sleep(2)
-        except:
-            print("errorSubject")
 
-        try:
             search = driver.find_element(
                 by=By.XPATH, value="//*[@aria-label = 'Message body']")
             search.clear()
@@ -146,10 +128,7 @@ def sendEmail(course, gradeValue):
             search.send_keys(Keys.CONTROL + "i")
             search.send_keys("This is an automated email")
             time.sleep(2)
-        except:
-            print("errorTextPaste")
 
-        try:
             search = driver.find_element(
                 by=By.XPATH, value="//*[@title = 'Send']")
             search.click()
@@ -158,11 +137,11 @@ def sendEmail(course, gradeValue):
             time.sleep(EMAIL_SEND_DELAY)
 
             print("Email sent!")
-        except:
-            print("errorSend")
+        except Exception as e:
+            print("Error sending email: " + str(e))
 
     return
 
 
 if __name__ == "__main__":
-    gradesCheck()
+    gradesCheck(courses)
