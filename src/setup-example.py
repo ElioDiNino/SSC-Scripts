7e7b1c232d17e323a357258726f788baee76b55d
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-07-04 10:21:13 -0700

    Add more configuration options

--- src/setup-example.py
+++ src/setup-example.py
@@ -1,4 +1,10 @@
 CWL = "example"
 PASSWORD = "password"
 EMAIL_LIST = ["example1@domain.com", "example2@domain.com "]
-CHECK_INTERVAL = 600
+# By default, UBC Webmail has a long delay before it sends an email so make sure this delay is long enough
+# that the email sends before the browser is closed.
+EMAIL_SEND_DELAY = 60
+CHECK_INTERVAL = 3600  # 1 hour
+# Choose whether the data found on the script runner's SSC will be included in the email.
+# Disable this if you are emailing multiple people and you want to keep your grades/specialization private.
+SEND_DATA = False
